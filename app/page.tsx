a1051d39bbb013b9d322b88ecfac05632f7a5674
Zi Sar K Nar <zisarkna@isgm2.com>
2024-01-08 20:20:21 +0900

    ref: coming soon

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,12 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col  justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
 
       <div>
-        <div className="lg:static">
+        <div className="flex flex-col justify-center items-center">
           <h2 className='uppercase font-semibold text-xl'>&lt;zi sar kay nar<span>&#47;</span>&gt;</h2>
-          <h6 className='capitalize font-light text-sm'>software engineer</h6>
+          <h6 className='capitalize font-light text-sm'>coming soon</h6>
         </div>
         
         <nav>
